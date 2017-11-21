631706c6720263af9404975a375c934e0b89cc1b
nlfox <nlfox@msn.cn>
2017-11-21 20:46:45 +0000

    update for mcrouter

--- client/test_client.py
+++ client/test_client.py
@@ -12,9 +12,11 @@ def send(message):
     sock.sendto(bytes(message), (socket.gethostbyname("nsl017"), 5005))
 
 
-def do(client_parameter, server_parameter):
+def do(client_parameter, server_parameter, tag="test", server_program="memcached"):
     client_parameter = Parameter(client_parameter)
+    client_parameter.setName("treadmill")
     server_parameter = Parameter(server_parameter)
+    server_parameter.setName(server_program)
     send(server_parameter.toJson())
     time.sleep(3)
     time_stamp = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
@@ -24,7 +26,8 @@ def do(client_parameter, server_parameter):
         shell=True)
     tmp_file = open("/tmp/" + time_stamp + ".txt").readlines()
     result = tmp_file[0]
-    Log.create(client_parameter=client_parameter.toJson(), server_parameter=server_parameter.toJson(), result=result)
+    Log.create(client_parameter=client_parameter.toJson(), server_parameter=server_parameter.toJson(), result=result,
+               tag=tag)
 
 
-do({"max_outstanding_requests": 1000}, {"t": 1})
+do({"max_outstanding_requests": 1000}, {"t": 1}, server_program="mcrouter")
